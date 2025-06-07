ab590da082b352e005a1ecc8cbee033c8ffb8d7f
Xgwareqdwg Zoikuxuiwo <anandranga038@outlook.com>
2025-06-07 00:47:32 +0000

    searches.py edited online with Bitbucket

--- src/searches.py
+++ src/searches.py
@@ -62,7 +62,7 @@ class Searches:
         self.browser = browser
         self.webdriver = browser.webdriver
         self.num_additional_searches = num_additional_searches
-        self.custom_search_limits = custom_search_limits or {"desktop": 10, "mobile": 5}
+        self.custom_search_limits = custom_search_limits or {"desktop": 5, "mobile": 5}
         self.use_custom_limits = use_custom_limits  # NEW: Switch
 
         # Device-specific shelf (UNCHANGED)
